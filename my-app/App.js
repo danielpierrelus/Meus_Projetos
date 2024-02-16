c51c8f796bbb63bd62a550c24717ba2604fc954d
danielpierrelus <pierrelusdaniel@gmail.com>
2024-02-16 20:00:56 -0300

    Terminando de gerar a senha

--- my-app/App.js
+++ my-app/App.js
@@ -1,6 +1,30 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native"
+import { Component, useState } from "react"
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal } from "react-native"
 import Slider from "@react-native-community/slider"
+import { ModalPassword } from './src/components/modal'
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
 export default function App(){
+  const [size, setSize] = useState(10)
+  const [passwordValue, setPasswordValue] = useState("")
+  const [modalVisible, setModalVisible] = useState(false)
+
+  function generatePassword(){
+    
+    let password = "";
+
+    for(let i = 0, n = charset.length; i < size; i++){
+      password += charset.charAt(Math.floor(Math.random() * n))
+    }
+
+    setPasswordValue(password)
+    setModalVisible(true);
+  }
+
+
+
+
   return(
     <View style ={styles.container}>
       <Image 
@@ -8,7 +32,7 @@ export default function App(){
        style={styles.logo}
       />
 
-      <Text style={styles.title}>20 caracteres</Text>
+      <Text style={styles.title}>{size} caracteres</Text>
 
       <View style={styles.area}>
         <Slider
@@ -18,12 +42,18 @@ export default function App(){
         maximumTrackTintColor="#FF0000"
         minimumTrackTintColor="#000"
         thumbTintColor="#392de9"
+        value={size}
+        onValueChange={ (value) => setSize(value.toFixed(0)) }
         />
       </View>
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={generatePassword}>
         <Text style={styles.buttonText}>Gerar senha</Text>
       </TouchableOpacity>
+
+      <Modal visible={modalVisible} animationType="fade" transparent={true} >
+        <ModalPassword password={passwordValue} handLeClose={ () => setModalVisible(false) }/>
+      </Modal>
 
     </View>
 
